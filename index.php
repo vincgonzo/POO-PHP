cbb9cfb2ff68cf133da0d7ade432b62e609f616b
vincent-castanie <vincgonzo@hotmail.fr>
2018-03-22 21:16:43 +0100

    static concept

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
-function chargerClasse($classe)
-{
-  require 'class/'.$classe . '.php'; // On inclut la classe correspondante au paramètre passé.
-}
-
-spl_autoload_register('chargerClasse');
+require 'autoload.php';
 
 
-$perso1 = new Personnage(10, 50); // Un premier personnage
-$perso2 = new Personnage(5, 20); // Un second personnage
+$perso1 = new Personnage(Personnage::FORCE_PETITE, 50); // Un premier personnage
+$perso2 = new Personnage(Personnage::FORCE_MOYENNE, 20); // Un second personnage
 
 $perso1->setForce(10);
 $perso1->setExperience(2);
@@ -25,3 +20,11 @@ $perso2->gagnerExperience(); // $perso2 gagne de l'expérience
 echo 'Le personnage 1 a ', $perso1->force(), ' de force, contrairement au personnage 2 qui a ', $perso2->force(), ' de force.<br />';
 echo 'Le personnage 1 a ', $perso1->experience(), ' d\'expérience, contrairement au personnage 2 qui a ', $perso2->experience(), ' d\'expérience.<br />';
 echo 'Le personnage 1 a ', $perso1->degats(), ' de dégâts, contrairement au personnage 2 qui a ', $perso2->degats(), ' de dégâts.<br />';
+
+Personnage::talk();
+
+$count1 = new Compteur();
+$count2 = new Compteur();
+$count3 = new Compteur();
+
+echo Compteur::getCompteur();
